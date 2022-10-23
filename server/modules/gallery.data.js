61774c1e595201ecc333b516ffe51d309bb6c1de
Daniel Pathammavong <patha037@umn.edu>
2022-10-23 12:33:56 -0500

    Added images

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -1,5 +1,7 @@
 const galleryItems = [
-    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 }
+    { id: 1, path: 'images/erock.jpg', description: 'Erock at Interstate State Park', likes: 0 },
+    {id:2, path: 'images/whip.jpg', description: 'Dashboard of classic car', likes: 0 },
+    {id:3, path: 'images/mpls.jpg', description: 'Droneshot of Minneapolis', likes: 0 },
+    {id:4, path: 'images/nipsey.jpg', description: 'Portrait of Nipsey Hussle', likes: 0 }
 ];
-
 module.exports = galleryItems;
